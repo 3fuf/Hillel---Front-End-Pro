0aa45169c53da9149115969ff6cd0d1b693ef3e7
3fuf <91079054+3fuf@users.noreply.github.com>
2022-01-16 15:56:52 +0200

    HW3z2

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-const userName = prompt('Как тебя зовут?');
-const userYear = prompt('Какого ты года рождения?');
-console.log(userName + (2022 - userYear));                                       //имя пользователя + год рожднния
+const firstNumber = 123;
+const secondNumber = 321;
 
-const variableA = prompt('Введите переменную а');
-const variableB = prompt('Введите переменную b');
-const variableC = prompt('Введите переменную C');
-console.log(Number(variableA)+Number(variableB)+Number(variableC));        //суммирования всех переменных
-console.log(Number(variableA) % 2 === 0);                                  //true - четные, false - не четные
-console.log(Number(variableB) % 2 === 0);                                  //
-console.log(Number(variableC) % 2 === 0);                                  //
-console.log((Number(variableA)+Number(variableB)+Number(variableC))/3);    //среднее арифметическое трех чисел
+if (firstNumber > secondNumber) {
+    console.log(firstNumber);
+} else {
+    console.log(secondNumber);
+}
 
-let n = 23456;
-let n1 = n % 10;
-n /= 10;
-n = Math.floor(n);
-let n2 = n % 10;
-n /= 10;
-n = Math.floor(n);
-let n3 = n % 10;
-n /= 10;
-n = Math.floor(n);
-let n4 = n % 10;
-n /= 10;
-n = Math.floor(n);
-let n5 = n % 10;
-n /= 10;
-n = Math.floor(n);
-console.log(n5 + ' ' + n4 + ' ' + n3 + ' ' + n2 + ' ' + n1 + ' ');                   //в исходном порядке через пробел
+
+let number = 1234;
+
+let numberOne = number % 10;
+number /= 10;
+number = Math.floor(number);
+let numberTwo = number % 10;
+number /= 10;
+number = Math.floor(number);
+let numberThree = number % 10;
+number /= 10;
+number = Math.floor(number);
+let numberFour = number % 10;
+number /= 10;
+number = Math.floor(number);
+
+console.log(numberOne >= 2);
+console.log(+numberOne);
+
