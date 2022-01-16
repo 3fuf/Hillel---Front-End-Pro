2fb1a8760f0ad3ca25385aeacdc6e1d9a8789618
3fuf <91079054+3fuf@users.noreply.github.com>
2022-01-16 14:32:45 +0200

    HW1

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Hello, World!");
+
+console.info("HELLO");
+
+console.error("Something went wrong");
+
+console.count("TEST");
+console.count("TEST");
+console.count("TEST");
+console.count("TEST");
+console.count("TEST");
+
+console.count("HELLO");
+console.count("HELLO");
+console.count("HELLO");
+
+console.time("REQUEsT");
+
+console.timeEnd("REQUEsT");
