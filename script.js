8ca6d937b8911f776292cb642ea7421b7abfbcef
3fuf <91079054+3fuf@users.noreply.github.com>
2022-01-16 15:20:26 +0200

    HW2

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-console.log("Hello, World!");
+const userName = prompt('Как тебя зовут?');
+const userYear = prompt('Какого ты года рождения?');
+console.log(userName + (2022 - userYear));                                       //имя пользователя + год рожднния
 
-console.info("HELLO");
+const variableA = prompt('Введите переменную а');
+const variableB = prompt('Введите переменную b');
+const variableC = prompt('Введите переменную C');
+console.log(Number(variableA)+Number(variableB)+Number(variableC));        //суммирования всех переменных
+console.log(Number(variableA) % 2 === 0);                                  //true - четные, false - не четные
+console.log(Number(variableB) % 2 === 0);                                  //
+console.log(Number(variableC) % 2 === 0);                                  //
+console.log((Number(variableA)+Number(variableB)+Number(variableC))/3);    //среднее арифметическое трех чисел
 
-console.error("Something went wrong");
-
-console.count("TEST");
-console.count("TEST");
-console.count("TEST");
-console.count("TEST");
-console.count("TEST");
-
-console.count("HELLO");
-console.count("HELLO");
-console.count("HELLO");
-
-console.time("REQUEsT");
-
-console.timeEnd("REQUEsT");
+let n = 23456;
+let n1 = n % 10;
+n /= 10;
+n = Math.floor(n);
+let n2 = n % 10;
+n /= 10;
+n = Math.floor(n);
+let n3 = n % 10;
+n /= 10;
+n = Math.floor(n);
+let n4 = n % 10;
+n /= 10;
+n = Math.floor(n);
+let n5 = n % 10;
+n /= 10;
+n = Math.floor(n);
+console.log(n5 + ' ' + n4 + ' ' + n3 + ' ' + n2 + ' ' + n1 + ' ');                   //в исходном порядке через пробел
