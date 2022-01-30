bde1dd1c452762b1286bf5a5088b614988929fb5
3fuf <91079054+3fuf@users.noreply.github.com>
2022-01-30 20:01:30 +0200

    HW4

--- script.js
+++ script.js
@@ -5,37 +5,107 @@ for (i = 10; i <= 20; i++) {
     str = str + i;
 }
 console.log(str);
+console.log(' ');
 
 console.log('№2');
 
-for (i = 10; i <= 20; i++) {
-    console.log(Math.pow(i, 2));
+for (j = 10; j <= 20; j++) {
+    console.log(Math.pow(j, 2));
 }
 console.log(' ');
 
 console.log('№3');
 
 let seven = 7;
-for (i = 1; i <= 10; i++) {
-    Number = i * seven;
+for (k = 1; k <= 10; k++) {
+    Number = k * seven;
     console.log(Number);
 }
+console.log(' ');
 
 console.log('№4');
 
 let count = 0;
-for (i = 1; i <= 15; i++) {
-    count += i;
+for (l = 1; l <= 15; l++) {
+    count += l;
 }
 console.log(count);
+console.log(' ');
 
 console.log('№5');
+
+let q = 1;
+for (p = 15; p <= 35; p++) {
+    q *= p
+}
+console.log(q);
+console.log(' ');
 
 console.log('№6');
 
 let means = 0;
-for (i = 1; i <= 500; i++) {
-    means += i;
+for (m = 1; m <= 500; m++) {
+    means += m;
 }
 console.log(means / 500);
+console.log(' ');
 
+console.log('№7');
+
+for (let n = 30; n <= 80; n++) {
+    if (n % 2 === 0) {
+        console.log(n);
+    }
+}
+console.log(' ');
+
+console.log('№8');
+
+for (let r = 100; r <= 200; r++) {
+    if (r % 3 === 0) {
+        console.log(r);
+    }
+}
+console.log(' ');
+
+console.log('№9');
+
+let numb = 22;
+let summ = 0;
+for (let s = 1; s <= numb; s++){
+    if(numb % s === 0 && numb !== numb && numb !== 1) {
+        if (s % 2 === 0) continue;
+        console.log(summ += s);
+
+    }
+    console.log(s);
+
+}
+
+console.log(' ');
+
+console.log('№10');
+
+for (let multTable = 1; multTable <= 10; multTable++) {
+    console.log('Таблица умножения на '+ multTable);
+
+    for (let multЕableМariable = 1; multЕableМariable <= 10; multЕableМariable++) {
+        console.log(multTable * multЕableМariable);
+    }
+}
+
+console.log(' ');
+
+console.log('№11');
+
+let guessNumber = 9;
+while (true) {
+    let value = +prompt('Угадай число от 0 до 10', '')
+    if (value === guessNumber) {
+        alert('Верно - это ' + guessNumber);
+        break;
+    }
+    if (!value) break; {
+        alert('Не верно - ' + value);
+    }
+}
