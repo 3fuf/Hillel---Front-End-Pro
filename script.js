20c2d128de9e0bbe1ad5289db085fd5afaabfa40
3fuf <91079054+3fuf@users.noreply.github.com>
2022-02-13 21:20:07 +0200

    HW6

--- script.js
+++ script.js
@@ -1,33 +1,70 @@
+//"№1"
 
-function calcArea(inpRadius) {
-    return Math.PI * inpRadius * inpRadius;
+const origin = {
+    firstName: 'Peter',
+    lastName: 'Parker',
+    email: 'test@test.com'
 }
-console.log(calcArea(7));
+const target = {};
 
-function calcCircle(inpRadius) {
-    return 2 * Math.PI * inpRadius;
-}
-console.log(calcCircle(5));
+const firstOb = {
+    name: 'Kolia',
+    email: 'test@test.com'
+};
 
-function calcAvarage (i, j) {
-    return (i + j) / 2;
-}
-console.log(calcAvarage(2, 3));
+const srcOb = {};
 
-function calc(x, y, action) {
-    switch (action) {
-        case '+':
-            return x + y;
-        case '-':
-            return x - y;
-        case '*':
-            return x * y;
-        case '/':
-            return x / y;
-        case '%':
-            return x % y;
-        case '^':
-            return x ** y;
+function copy(x, y) {
+    for (const key in x) {
+        y[key] = x[key];
     }
+    return y;
 }
-console.log(calc(2, 1, '+'));
+console.log(copy(firstOb, srcOb))
+console.log(copy(origin, target))
+
+//"№2"
+
+function isEqual(x, y) {
+    const list1 = Object.getOwnPropertyNames(x);
+    const list2 = Object.getOwnPropertyNames(y);
+
+    if (list1.length !== list2.length) {
+        return false;
+    }
+
+    for (let i = 0; i < list2.length; i += 1) {
+        const list = list1[i];
+
+        if (x[list] !== y[list]) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(isEqual(origin, target));
+console.log(isEqual(origin, srcOb));
+
+//"№3"
+
+function fn(str) {
+    const hash = {};
+    for(let i = 0; i < str.length; i++) {
+      if(hash[str[i]]) {
+        hash[str[i]]++;
+      } else {
+        hash[str[i]] = 1;
+      }
+    }
+    let keys = Object.keys(hash);
+    let maxValue = 1;
+    let maxKey = '';
+    for(let i = 0; i < keys.length; i++) {
+      if(hash[keys[i]] > maxValue) {
+        maxValue = hash[keys[i]];
+        maxKey = keys[i];
+      }
+    }
+    console.log(maxKey, maxValue);
+  }
+  fn('aaaaabbbcccc')
