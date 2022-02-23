aa657562b7bb7438661df0620c4589da16231b54
3fuf <91079054+3fuf@users.noreply.github.com>
2022-02-23 17:50:48 +0200

    HW6

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//"№1"
+console.log("№1")
 
 const origin = {
     firstName: 'Peter',
@@ -23,48 +23,32 @@ function copy(x, y) {
 console.log(copy(firstOb, srcOb))
 console.log(copy(origin, target))
 
-//"№2"
+console.log("№2")
 
 function isEqual(x, y) {
-    const list1 = Object.getOwnPropertyNames(x);
-    const list2 = Object.getOwnPropertyNames(y);
+    const list1 = Object.keys(x).length;
+    const list2 = Object.keys(y).length;
 
-    if (list1.length !== list2.length) {
-        return false;
+    for (const key in x) {
+      return true;
     }
-
-    for (let i = 0; i < list2.length; i += 1) {
-        const list = list1[i];
-
-        if (x[list] !== y[list]) {
-            return false;
-        }
-    }
-    return true;
+    return false;
 }
-console.log(isEqual(origin, target));
+console.log(isEqual(org = {}, org1 = {name: "Peater"}));
 console.log(isEqual(origin, srcOb));
 
-//"№3"
+console.log("№3")
 
 function fn(str) {
-    const hash = {};
-    for(let i = 0; i < str.length; i++) {
-      if(hash[str[i]]) {
-        hash[str[i]]++;
-      } else {
-        hash[str[i]] = 1;
-      }
+  const hash = {};
+  for(let i = 0; i < str.length; i++) {
+    if(hash[str[i]]) {
+      hash[str[i]]++;
+    } else {
+      hash[str[i]] = 1;
     }
-    let keys = Object.keys(hash);
-    let maxValue = 1;
-    let maxKey = '';
-    for(let i = 0; i < keys.length; i++) {
-      if(hash[keys[i]] > maxValue) {
-        maxValue = hash[keys[i]];
-        maxKey = keys[i];
-      }
-    }
-    console.log(maxKey, maxValue);
   }
-  fn('aaaaabbbcccc')
+  return hash;
+}
+
+console.log(fn('aaaaabbbcccc'));
