b8237294ee541f2e3a9f54ea7ee2998cfad206bd
3fuf <91079054+3fuf@users.noreply.github.com>
2022-04-12 10:45:53 +0300

    HW7

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
+console.log("№1")
 
 function isSymbolPresentInString(str,symbol) {
   for (let i = 0; i < str.length; i++) {
     if (str[i] === symbol) {
       return true;
-      break;
     }
   }
   return false;
@@ -13,13 +13,12 @@ console.log(isSymbolPresentInString("abc", "a"));
 console.log(isSymbolPresentInString("abc", "e"));
 
 
-console.log("***")
+console.log("№2")
 
 function getSymbolIndex(str,symbol) {
   for (let i = 0; i < str.length; i++) {
     if (str[i] === symbol) {
       return i;
-      break;
     }
   }
   return -1;
@@ -28,7 +27,7 @@ console.log(getSymbolIndex("hello lol","h"));
 console.log(getSymbolIndex("hello lol","l"));
 console.log(getSymbolIndex("hello lol","v"));
 
-console.log("***")
+console.log("№3")
 
 function getNumberOfEven(n) {
   const str = String(n);
@@ -44,25 +43,23 @@ function getNumberOfEven(n) {
 console.log(getNumberOfEven(223344));
 console.log(getNumberOfEven(111));
 
-console.log("***")
+console.log("№4 forEach")
 
-const oneByOne = function(arr, callback) {
-  length = arr.length;
-  for ( i = 0; i < length; i++) {
+const forEach = function(arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
     callback(arr[i], i, arr)
   }
 }
 
-oneByOne([10, 11, 12, 13, 14, 15], function (n, i, numb) {
+forEach([10, 11, 12, 13, 14, 15], function (n, i, numb) {
   console.log(i, n, numb)
 });
 
-console.log("***")
+console.log("№5 map")
 
 const map = function (arr, callback, thisArg) {
-  length = arr.length,
   result = [];
-  for (i = 0; i < length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     result.push(callback.call(thisArg, arr[i]));
   }
   return result;
@@ -74,10 +71,57 @@ const circumcision = map(['Peater1', 'Peater 2', 'Peater 3'], function(circumcis
 
 console.log(circumcision);
 
-console.log("***")
+console.log("№6 filter")
 
-const isEvenNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].filter(function(n) {
+const filter = function (arr, callback, thisArg) {
+  result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (callback.call(thisArg, arr[i], i, arr)) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+
+const isEvenNumbers = filter([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], function(n) {
   return (n % 2 === 0);
 });
 
 console.log(isEvenNumbers);
+
+console.log("№7 every")
+
+const every = function (arr, callback, thisArg) {
+  for (let i = 0; i < arr.length; i++) {
+    if (!callback.call(thisArg, arr[i], i, arr)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+const isEvenNumb = every([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], function(n) {
+  return (n % 2 === 0);
+});
+
+console.log(isEvenNumb);
+
+console.log("№8 some")
+
+const some = function (arr, callback, thisArg) {
+  for (let i = 0; i < arr.length; i++) {
+    if (callback.call(thisArg, arr[i], i, arr)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const isSeven = some(num, function(n) {
+  return num < 7;
+});
+
+console.log(isSeven);
