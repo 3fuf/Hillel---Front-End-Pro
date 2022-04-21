caf938f82fdfad710a4a3136cf89f6b51e9b8443
Andrii <a.nasushchnyi@mriyadev.com>
2022-04-21 16:20:36 +0300

    HW10

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-function isSum() {
-	let resault = 0;
-	return function(num) {
-		return resault += num;
-	}
+function generateList(arr) {
+	const ul = document.createElement("ul");
+
+	arr.forEach(function(element){
+		const li = document.createElement("li");
+
+		if(Array.isArray(element)) {
+			childElement = generateList(element);
+		} else {
+			childElement = document.createTextNode(element)
+		}
+		li.appendChild(childElement);
+		ul.appendChild(li);
+	});
+	return ul;
 }
 
-const sum = isSum();
-
-console.log(sum(3), sum(5), sum(20));
+document.body.appendChild(generateList([1, 2, 3]));
+document.body.appendChild(generateList([1, 2, [1.1, 1.2, 1.3], 3]));
